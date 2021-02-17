3fbf4ab90eeb51bf693cd76dbc585e7e922d107d
Anitha Venkatesan <anithamca68@gmail.com>
2021-02-16 23:20:32 -0600

    Added quantity, price and add to cart button

--- src/ProductList.js
+++ src/ProductList.js
@@ -9,33 +9,46 @@ import list5 from "./images/SW1613.jpg";
 const products = [
   {
     images: list1,
-    text: "Vitamin3",
+    text: "Vitamin3 - Highest Potency",
+    quantity: "5,000 IU 250 Sgels",
+    price: "$10.99"
   },
   {
     images: list2,
-    text: "Vitamin3",
+    text: "Zinc Picolinate - Body Preferred Form",
+    quantity: "22 mg 60 Caps",
+    price: "2.59"
   },
   {
     images: list3,
-    text: "Vitamin3",
+    text: "Dr.Stephen Langer's Ultimate 16 Strain Probiotic with FOS",
+    quantity: "3.2 Billion CFU 60 Veg Drcaps",
+    price: "2 for $14.79"
   },
   {
     images: list4,
-    text: "Vitamin3",
+    text: "Delivery Insurance",
+    
   },
   {
     images: list5,
-    text: "Vitamin3",
+    text: "Turmeric and Back Pepper",
+    quantity: "60 Veg Caps",
+    price: "2 for $13.99"
   }
 ]
 class ProductList extends React.Component {
   render() {
     return (
-      <div>
+      <div className = "container">
         {products.map((product, index) => (
-          <grid key={index}>
-            <img src={product.images} alt="productimages" style={{ width: 250 }} />
-          </grid>
+          <div key={index} >
+            <img src={product.images} alt="productimages"  />
+            <div>{product.text}</div>
+            <div>{product.quantity}</div>
+            <div>{product.price}</div>
+            <button className="buttonColor">Add to cart</button>
+          </div>
         )
         )}
       </div>
