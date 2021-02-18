3880c823b0c789429ea46630cf6cdcf77ee74bd2
Anitha Venkatesan <anithamca68@gmail.com>
2021-02-17 21:02:56 -0600

    Added flex grow property for container

--- src/Button.js
+++ src/Button.js
@@ -2,15 +2,11 @@ import React from "react";
 import "./Button.css";
 
 class Button extends React.Component {
-
-  render(){
-    return(
-      <div>
+  render() {
+    return (
         <button className="buttonColor">add to cart</button>
-      </div>
     )
   }
-
 }
 
 export default Button;
--- src/ProductList.js
+++ src/ProductList.js
@@ -10,21 +10,21 @@ import list5 from "./images/SW1613.jpg";
 
 const products = [
   {
-    images: list1,
+    image: list1,
     text: "Vitamin3 - Highest Potency",
     quantity: "5,000 IU 250 Sgels",
     premium:"Swanson Premium",
     price: "$10.99"
   },
   {
-    images: list2,
+    image: list2,
     text: "Zinc Picolinate - Body Preferred Form",
     quantity: "22 mg 60 Caps",
     premium:"Swanson Premium",
     price: "$2.59"
   },
   {
-    images: list3,
+    image: list3,
     text: "Dr.Stephen Langer's Ultimate 16 Strain Probiotic with FOS",
     quantity: "3.2 Billion CFU 60 Veg Drcaps",
     premium:"Swanson Probiotics",
@@ -32,7 +32,7 @@ const products = [
 
   },
   {
-    images: list4,
+    image: list4,
     text: "DELIVERY INSURANCE",
     premium:"Swanson Health Prodcuts, Inc.",
     price: "$1.00"
@@ -40,7 +40,7 @@ const products = [
   },
 
   {
-    images: list5,
+    image: list5,
     text: "Turmeric and Back Pepper",
     quantity: "60 Veg Caps",
     premium:"Swanson Premium",
@@ -49,26 +49,25 @@ const products = [
 ]
 class ProductList extends React.Component {
   render() {
-    return (
-      <div className= "main">
+    return <div className='container'>
+      <ul className='list'>
         {products.map((product, index) => (
-          <div key={index} className="container">
-            <div className="info">
-              <img className='info-image' src={product.images} alt="productimages" />
-              <div className="premium">{product.premium}</div>
-              <div className="text">{product.text}</div>
+          <li className='list-item' key={index}>
+            <div className='list-item-child'>
+              <img src={product.image} alt={product.text} />
             </div>
-            <div className= "cartContainer">
-            <div className="quantity">{product.quantity}</div>
-            <div className="price">{product.price}</div>
-            <Button />
+            <div className='list-item-child'>Reviews section</div>
+            <div className='list-item-child premium'>{product.premium}</div>
+            <div className='list-item-child text'>{product.text}</div>
+            <div className='list-item-child quantity'>{product.quantity}</div>
+            <div className='list-item-child price'>{product.price}</div>
+            <div className='list-item-child'>
+              <Button />
             </div>
-          </div>
-        )
-        )}
-      </div>
-
-    );
+          </li>
+        ))}
+      </ul>
+    </div>
   }
 }
 export default ProductList;
