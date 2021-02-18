54988f1e81d6915c098aa50e38172a3921116bd1
Anitha Venkatesan <anithamca68@gmail.com>
2021-02-17 21:17:29 -0600

    Created componnet for Shopnow button

--- src/ShopNow.js
+++ src/ShopNow.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "./ShopNow.css";
+import ShopNowButton from "./ShopNowButton.js";
 import list1 from "./images/SW1371.jpg";
 import list2 from "./images/SW1113.jpg";
 import list3 from "./images/SWA051.jpg";
 
 const medicine = [
   {
-    images: list1,
+    image: list1,
     name: "Healthy Joints, Digestion & More",
 
   },
   {
-    images: list2,
+    image: list2,
     name: "Mighty Magnesium",
     description: "Essential mineral for healthy bones, heart & so much more",
 
   },
   {
-    images: list3,
+    image: list3,
     name: "Prebiotics & Probiotics",
     description: "A dynamic duo for gut health",
 
@@ -27,19 +28,27 @@ const medicine = [
 class Shopnow extends React.Component {
   render() {
     return (
-      <div className="boxContainer">
-        {medicine.map((med, index) => (
-          <div key={index} className="shopContainer">
-            <img src={med.images} alt="productimages" />"
-            <div className="name">{med.name}</div>
-            <div>{med.description}</div>
-            <button className="shopnow">shop now</button>
-          </div>
-        )
-        )}
-      </div>
-
-    );
+      <div className="container">
+        <ul className='list'>
+          {medicine.map((med, index) => (
+            <li className='list-item' key={index}>
+              <div>
+              <div className='list-item-child'>
+                <img src={med.image} alt={med.text} />
+              </div>
+              <div>
+              <div className='list-item-child name'>{med.name}</div>
+              <div className='list-item-child description'>{med.description}</div>
+              <div className='list-item-child'>
+                <ShopNowButton />
+              </div>
+              </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+     </div>
+    )
   }
 }
 export default Shopnow;
---
+++ src/ShopNowButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ShopNowButton.css";
+
+class Button extends React.Component {
+  render() {
+    return (
+        <button className="shopNowButtonColor">shop now</button>
+    )
+  }
+}
+
+export default Button;
